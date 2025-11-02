818d59d2ac1d141cdd22562b60260b328bd526ca
Tony-jdev <artpopko28@gmail.com>
2025-11-02 20:30:59 +0200

    Added feedback page

--- Лабораторна робота 5/encyclopedia/src/app/app.routes.ts
+++ Лабораторна робота 5/encyclopedia/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import {LayoutComponent} from './layout/layout.component';
 import {HomeComponent} from './pages/home/home.component';
 import {CarnivorousDinosaursComponent} from './pages/carnivorous-dinosaurs/carnivorous-dinosaurs.component';
+import {FeedbackComponent} from './pages/feedback/feedback.component';
 
 export const routes: Routes = [
   {
@@ -10,6 +11,7 @@ export const routes: Routes = [
     children: [
       { path: '', component: HomeComponent },
       { path: 'some1', component: CarnivorousDinosaursComponent },
+      { path: 'feedback', component: FeedbackComponent }
     ]
   }
 ];
---
+++ Лабораторна робота 5/encyclopedia/src/app/pages/feedback/feedback.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-feedback',
+  imports: [
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './feedback.component.html',
+  styleUrl: './feedback.component.css',
+})
+export class FeedbackComponent {
+  feedbackForm: FormGroup;
+  showTooltip = false;
+
+  constructor(private fb: FormBuilder) {
+    this.feedbackForm = this.fb.group({
+      fullname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      age: [null, [Validators.required, Validators.min(5), Validators.max(120)]],
+      education: ['', Validators.required],
+      purpose: ['', Validators.required],
+      details: ['', [Validators.required, Validators.maxLength(500)]],
+      consent: [false, Validators.requiredTrue]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.feedbackForm.invalid) {
+      this.feedbackForm.markAllAsTouched();
+      alert('Будь ласка, заповніть усі обов’язкові поля.');
+      return;
+    }
+
+    console.log('Форма відправлена:', this.feedbackForm.value);
+    alert('Дякуємо! Ваш відгук успішно відправлено.');
+
+    this.feedbackForm.reset();
+  }
+
+  get f() {
+    return this.feedbackForm.controls;
+  }
+}
